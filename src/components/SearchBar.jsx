b5edbae9207ed964aca0f29f699a0dc8f0a312c5
Danny Chen <dannychen@Dannys-MacBook-Air.local>
2025-01-14 10:55:11 -0500

    cleanup SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,8 @@
 import React from 'react';
-import { fetchWeatherApi } from 'openmeteo';
 import { useState, useEffect } from 'react';
 import ResultDropdown from './ResultDropdown';
 
 const SearchBar = ({ setWeatherData }) => {
-  const weatherFetch = async (e) => {
-    e.preventDefault();
-    console.log('submit');
-    
-    try {
-      // const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${locationData.latitude}&longitude=${locationData.longitude}&current=temperature_2m,apparent_temperature,weather_code,wind_speed_10m&hourly=temperature_2m,precipitation_probability&daily=weather_code,temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&timezone=auto`
-      const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${locationData.latitude}&longitude=${locationData.longitude}&current=temperature_2m,apparent_temperature,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code,precipitation_probability&daily=weather_code,temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&timezone=auto`
-      const res = await fetch(weatherUrl);
-      const data = await res.json();
-
-      // Add location name to JSON
-      data.name = locationData.name;
-      setWeatherData(data);
-      console.log(data);
-
-    } catch (error) {
-      console.log('Error fetching weather data', error);
-    }
-  }
-
   const [data, setData] = useState([]); // State to hold JSON data
   const [locationData, setLocationData] = useState(); // Save specific location for fetching weather
   const [query, setQuery] = useState(''); // Save search input
@@ -38,6 +17,7 @@ const SearchBar = ({ setWeatherData }) => {
 				const data = await res.json();
 
         Array.isArray(data.results) ? setData(data.results) : setData([]);
+
 			} catch (error) {
 				console.log('Error fetching geocoding data', error);
 			}
@@ -50,17 +30,46 @@ const SearchBar = ({ setWeatherData }) => {
   // Update filtered results when query or data changes
   useEffect(() => {
     if (query) {
-      console.log(data);
       const results = data.filter((item) =>
         item.name.toLowerCase().includes(query.toLowerCase())
       );
 
       setFilteredResults(results);
-
-    } else {
+    } 
+    else {
       setFilteredResults([]);
     }
   }, [query, data]);
+
+  const weatherFetch = async (e) => {
+    e.preventDefault();
+    console.log('submit');
+    
+    try {
+      const weatherUrl = `
+        https://api.open-meteo.com/v1/forecast?
+        latitude=${locationData.latitude}&
+        longitude=${locationData.longitude}&
+        current=temperature_2m,apparent_temperature,weather_code,wind_speed_10m&
+        hourly=temperature_2m,weather_code,precipitation_probability&
+        daily=weather_code,temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_probability_max&
+        temperature_unit=fahrenheit&
+        wind_speed_unit=mph&
+        timezone=auto
+      `.replace(/\s/g, '');
+      
+      const res = await fetch(weatherUrl);
+      const data = await res.json();
+
+      // Add location name to JSON
+      data.name = locationData.name;
+      setWeatherData(data);
+      console.log(data);
+
+    } catch (error) {
+      console.log('Error fetching weather data', error);
+    }
+  }
 
   return (
     <div className='relative max-w-md mx-auto text-black'>
