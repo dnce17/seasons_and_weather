19d9392f964a718e126d3cc3f33f8df98cd34a2a
Danny Chen <dannychen@Dannys-Air.lan1>
2025-01-21 14:44:24 -0500

    refactor: useEffect + useState for bg change

--- src/pages/WeatherPage.jsx
+++ src/pages/WeatherPage.jsx
@@ -1,42 +1,43 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar';
 import TemperatureToday from '../components/TemperatureToday';
 import ForecastToday from '../components/ForecastToday';
 import CurrentConditions from '../components/CurrentConditions';
 import ForecastFuture from '../components/ForecastFuture';
-import { weatherCategories } from '../../helpers';
 
+import { weatherCategories } from '../../helpers';
 import DefaultBg from '../assets/imgs/partly_cloudy_bg.jpg';
 
 // https://api.open-meteo.com/v1/forecast?latitude=40.7684&longitude=-73.7771&current=temperature_2m,apparent_temperature,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code,precipitation_probability&daily=weather_code,temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&timezone=auto
 
 const WeatherPage = () => {
   const [weatherData, setWeatherData] = useState();
+  const [weatherBg, setWeatherBg] = useState(DefaultBg);
 
-  const getBg = () => {
-    if (weatherData) {
-      for (const item in weatherCategories) {
-        // Cycle through weather code to determine which bg to use
-        for (const code of weatherCategories[item].codes) {
-          // TEST item
-          // if (73 == code) { 
-          if (weatherData.current.weather_code == code) {
-            return weatherCategories[item].bg;
+  useEffect(() => {
+    const getBg = () => {
+      if (weatherData) {
+        for (const item in weatherCategories) {
+          // Cycle through weather code to determine which bg to use
+          for (const code of weatherCategories[item].codes) {
+            // if (71 == code) {  // TEST USE
+            if (weatherData.current.weather_code == code) {
+              setWeatherBg(weatherCategories[item].bg);
+              return true;
+            }
           }
         }
       }
+      return false;
     }
-    else {
-      return DefaultBg;
-    }
-  }
 
-  let weatherBg = getBg();
+    getBg();
+
+  }, [weatherData])
 
   return (
-    // <div className={`bg-cover text-slate-200`} style={{ backgroundImage: `url(${bgClass})` }} >
-    <div className='bg-cover text-slate-200' style={{ backgroundImage: `url(${weatherBg})` }} >
+    <div className='bg-cover text-slate-200 transition-all duration-400 opacity-100' style={{ backgroundImage: `url(${weatherBg})` }} >
       <div className='bg-slate-700 bg-opacity-30 py-8'>
         <SearchBar setWeatherData={setWeatherData} />
       </div>
